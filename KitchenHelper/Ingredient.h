cdc2000d5df60d79232160d4bd9eb62522aa48ac
fieddinsum <150everyday@gmail.com>
2015-09-20 16:47:39 +0900

    Merge branch 'InterfaceUpdateCode'
    Conflicts:
    	KitchenHelper.sdf
    	KitchenHelper.v12.suo
    	KitchenHelper/DataBase.h
    	KitchenHelper/Ingredient.h
    	KitchenHelper/Recipe.h

--- KitchenHelper/Ingredient.h
+++ KitchenHelper/Ingredient.h
@@ -1,17 +1,24 @@
+/*
+ * 레시피에 들어갈 재료.
+ */
+
 #pragma once
+#include<iostream>
 
-
-
+using namespace std;
 
 class Ingredient{
 
-
 private:
-	char* ingredientName;		//재료명
-	int amount;					//재료양
+	char* ingredientName;					//재료명
+	int amount;								//재료량
 public:
-	char* getName();			//재료이름반환
-	void setName();				//재료명 얻기 
-	int getAmount();			//재료량얻기
-	void setAmount();
+	Ingredient(char* name, int amount);		//생성자
+											//재료를 입력받을 때, 이름과 양을 입력받음
+	char* getIngredientName();				//재료명을 반환함
+	int getAmount();						//재료량을 반환함
+
+	void setName(char* name);							//재료명을 setting(수정 시)
+	void setAmount(int amount);						//재료량을 setting(수정 시)
+
 };
